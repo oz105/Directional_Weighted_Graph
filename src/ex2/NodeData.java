a34e8d6d0fb71c2b52166067976ff390e9e27d36
oz105 <ozzz105@gmail.com>
2020-11-30 17:10:11 +0200

    update

---
+++ src/ex2/NodeData.java
@@ -0,0 +1,117 @@
+package ex2;
+
+public class NodeData implements node_data {
+    private int key;
+    private int tag;
+    private String info;
+    private double weight;
+
+    //CONSTRUCTOR
+    public NodeData(int key) {
+        this.key = key;
+        this.tag = -1;
+        this.info = "";
+        this.weight = 0.0;
+    }
+    //COPY CONSTRUCTOR
+    public NodeData(node_data n) {
+        this.key = n.getKey();
+        this.tag = n.getTag();
+        this.info = n.getInfo();
+        this.weight = n.getWeight() ;
+    }
+
+    /**
+     * Returns the key (id) associated with this node.
+     *
+     * @return
+     */
+    @Override
+    public int getKey() {
+        return this.key;
+    }
+
+    /**
+     * Returns the location of this node, if
+     * none return null.
+     *
+     * @return
+     */
+    @Override
+    public geo_location getLocation() {
+        return null;
+    }
+
+    /**
+     * Allows changing this node's location.
+     *
+     * @param p - new new location  (position) of this node.
+     */
+    @Override
+    public void setLocation(geo_location p) {
+
+    }
+
+    /**
+     * Returns the weight associated with this node.
+     *
+     * @return
+     */
+    @Override
+    public double getWeight() {
+        return this.weight;
+    }
+
+    /**
+     * Allows changing this node's weight.
+     *
+     * @param w - the new weight
+     */
+    @Override
+    public void setWeight(double w) {
+        this.weight = w;
+    }
+
+    /**
+     * Returns the remark (meta data) associated with this node.
+     *
+     * @return
+     */
+    @Override
+    public String getInfo() {
+        return this.info;
+    }
+
+    /**
+     * Allows changing the remark (meta data) associated with this node.
+     *
+     * @param s
+     */
+    @Override
+    public void setInfo(String s) {
+        this.info = s;
+    }
+
+    /**
+     * Temporal data (aka color: e,g, white, gray, black)
+     * which can be used be algorithms
+     *
+     * @return
+     */
+    @Override
+    public int getTag() {
+        return this.tag;
+    }
+
+    /**
+     * Allows setting the "tag" value for temporal marking an node - common
+     * practice for marking by algorithms.
+     *
+     * @param t - the new value of the tag
+     */
+    @Override
+    public void setTag(int t) {
+        this.tag = t;
+    }
+}
+
