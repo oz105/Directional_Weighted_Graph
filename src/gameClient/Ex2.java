9ac8c0beca6212c044bb3a4c361e7d3326d78e5f
ShaniShuv <74142432+ShaniShuv@users.noreply.github.com>
2020-12-09 16:30:40 +0200

    commit

---
+++ src/gameClient/Ex2.java
@@ -0,0 +1,22 @@
+//package gameClient;
+//
+//import Server.Game_Server_Ex2;
+//import api.game_service;
+//
+//import javax.swing.*;
+//
+//public class Ex2 {
+//    public static void main (){
+////        Arena ar = new Arena();
+//        MyFrame frame = new MyFrame(String s);
+//        frame.setSize(500, 500);
+//        frame.setVisible(true);
+//
+//
+//        int levelNum = 0;
+//        game_service game = Game_Server_Ex2.getServer(levelNum);
+//
+//    }
+//
+//
+//}
